8f010372e5bf1018ed3642432812995da4f00dfc
unknown <pal.iasmina@yahoo.com>
2022-06-04 13:40:05 +0100

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn italianissimo.wsgi
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://ecsmgubokpchfh:e650d0862fde9d2977cc4f6f1fec750dbc138408ea4166591dc8aea14f3cc1a4@ec2-52-18-116-67.eu-west-1.compute.amazonaws.com:5432/d5jo4hdje65j34"
+os.environ["SECRET_KEY"] = "randomSecretKey2022!!"
+os.environ["CLOUDINARY_URL"] = "cloudinary://327156977212439:2Kil58oNWYLIwbFaKH1UP5KzAyo@useriasminna"
