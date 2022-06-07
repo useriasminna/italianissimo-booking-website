e4831d175225067e8af90f5d736d4e37e8c7737c
unknown <pal.iasmina@yahoo.com>
2022-06-07 12:03:07 +0100

    Update manage.py app name

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Italianissimo.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'italianissimo.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
