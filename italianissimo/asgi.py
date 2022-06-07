4cac281cfba633fe6e02148343313dc250e93b85
unknown <pal.iasmina@yahoo.com>
2022-06-07 11:56:36 +0100

    Update app files with new app name

--- italianissimo/asgi.py
+++ italianissimo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Italianissimo project.
+ASGI config for italianissimo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Italianissimo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'italianissimo.settings')
 
 application = get_asgi_application()
--- italianissimo/wsgi.py
+++ italianissimo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Italianissimo project.
+WSGI config for italianissimo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
