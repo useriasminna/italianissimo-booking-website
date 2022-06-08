72a006241e89d6824b115c463f82d577d44f5c4a
unknown <pal.iasmina@yahoo.com>
2022-06-08 15:18:11 +0100

    Update view for index.html template

--- home/views.py
+++ home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
-class BaseTemplate(View):
-    template_name = "base.html"
+class Home(View):
+    template_name = "index.html"
