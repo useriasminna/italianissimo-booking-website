f9512be2ef455353d5adb4c680e448680b080d4a
unknown <pal.iasmina@yahoo.com>
2022-06-08 15:44:49 +0100

    Update Home class with TemplateView args

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 
 # Create your views here.
-class Home(View):
+class Home(TemplateView):
     template_name = "index.html"
