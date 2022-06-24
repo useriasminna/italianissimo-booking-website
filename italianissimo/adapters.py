b692382d93501fcb2de2d4eaf80f926c2c3a0b2c
unknown <pal.iasmina@yahoo.com>
2022-06-24 22:20:06 +0100

    Add adapter to match the custom registration form

---
+++ italianissimo/adapters.py
@@ -0,0 +1,15 @@
+from allauth.account.adapter import DefaultAccountAdapter
+
+class AccountAdapter(DefaultAccountAdapter):
+    def save_user(self, request, user, form, commit=True):
+        data = form.cleaned_data
+        user.email = data['email']
+        user.first_name = data['first_name']
+        user.last_name = data['last_name']
+        if 'password1' in data:
+            user.set_password(data['password1'])
+        else:
+            user.set_unusable_password()
+        if commit:
+            user.save()
+        return user
