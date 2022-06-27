0ce295f46c0abc4202dbf8028a4513ad2b378e01
unknown <pal.iasmina@yahoo.com>
2022-06-27 15:17:36 +0100

    Add permisions to custom user model

--- users/models.py
+++ users/models.py
@@ -43,9 +43,9 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
-    email        = models.EmailField(verbose_name='email address', unique=True)
-    first_name   = models.CharField(max_length=30)
-    last_name    = models.CharField(max_length=30)
+    email = models.EmailField(verbose_name='email address', unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
     active = models.BooleanField(default=True)
     staff = models.BooleanField(default=False)
     admin = models.BooleanField(default=False)
@@ -74,3 +74,9 @@ class User(AbstractBaseUser):
     @property
     def is_active(self):
         return self.active 
+    
+    def has_perm(self, perm, obj=None):
+        return self.staff
+
+    def has_module_perms(self, app_label):
+        return self.staff
