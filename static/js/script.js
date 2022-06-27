e00d42208fdbf1c85920f329d63d6d5bec1f1549
unknown <pal.iasmina@yahoo.com>
2022-06-27 13:34:28 +0100

    Add script for buttom to top button

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
