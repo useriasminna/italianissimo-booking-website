035809749f378412f6f3a93ce21ed354420f315e
unknown <pal.iasmina@yahoo.com>
2022-06-28 15:59:25 +0100

    Make bottom to top button script render only when in index page

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,20 @@
-//Get the button
-let mybutton = document.getElementById("btn-back-to-top");
 
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
+document.addEventListener("DOMContentLoaded", function(event) { 
+  if (window.location.pathname=='/') {
 
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+    // --------------------------ADD EVENT LISTENER FOR BOTTOM TO TOP BUTTON ON INDEX PAGE--------------------------
+    //Get the button
+    let mybutton = document.getElementById("btn-back-to-top");
+
+    // When the user clicks on the button, scroll to the top of the document
+    mybutton.addEventListener("click", backToTop);
+
+    function backToTop() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+
+  }
+  
+
+});
