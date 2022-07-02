b767901b31d9327f14828f36344f377712d798cf
unknown <pal.iasmina@yahoo.com>
2022-07-02 10:02:57 +0100

    Update booking url

--- booking/urls.py
+++ booking/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('/booking', views.Booking.as_view(), name='booking'),
+    path('', views.Booking.as_view(), name='booking'),
 ]
