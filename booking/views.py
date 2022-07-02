8e75bdb4b4cb5ad2c454be07fd1997d37fd92d1a
unknown <pal.iasmina@yahoo.com>
2022-07-02 10:06:54 +0100

    Add booking form to booking class view context

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,28 @@
 from django.views.generic import TemplateView
+from .forms import newBookingForm
+from django.shortcuts import render
 
 
 # Create your views here.
 class Booking(TemplateView):
     template_name = "booking.html"
+    
+    def post(self, request, slug, *args, **kwargs):
+        
+
+    #     # booking_form = newBookingForm(data=request.POST)
+
+    #     # if booking_form.is_valid():
+    #     #     booking_form.instance.email = request.user.email
+    #     #     booking_form.instance.name = request.user.username
+    #     #     booking = booking_form.save(commit=False)
+    #     #     booking.save()
+    #     # else:
+    #     #     booking_form = newBookingForm()    
+
+        return render(
+            request,
+            "booking.html",
+            {
+                'booking_form': newBookingForm()
+            })
