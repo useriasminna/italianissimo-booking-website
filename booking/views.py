6fbe9cae77253cd751fa0d67843471ac4330f25f
unknown <pal.iasmina@yahoo.com>
2022-07-04 16:15:08 +0100

    Override get_context for booking view

--- booking/views.py
+++ booking/views.py
@@ -1,28 +1,18 @@
 from django.views.generic import TemplateView
 from .forms import newBookingForm
+from .models import Booking
 from django.shortcuts import render
+from django.views.generic.edit import FormView
 
 
 # Create your views here.
 class Booking(TemplateView):
+
     template_name = "booking.html"
     
-    def post(self, request, slug, *args, **kwargs):
-        
-
-    #     # booking_form = newBookingForm(data=request.POST)
-
-    #     # if booking_form.is_valid():
-    #     #     booking_form.instance.email = request.user.email
-    #     #     booking_form.instance.name = request.user.username
-    #     #     booking = booking_form.save(commit=False)
-    #     #     booking.save()
-    #     # else:
-    #     #     booking_form = newBookingForm()    
-
-        return render(
-            request,
-            "booking.html",
-            {
-                'booking_form': newBookingForm()
-            })
+    def get_context_data(self,*args, **kwargs):
+        context = super(Booking, self).get_context_data(*args,**kwargs)
+        context['form'] = newBookingForm()
+        return context
+    
+  
