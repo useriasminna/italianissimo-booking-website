410647ad91f29f830ff892957e74afb29f43f9f0
unknown <pal.iasmina@yahoo.com>
2022-07-07 09:23:32 +0100

    Comment table field because table selection has not been implemented yet

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,4 @@
+from operator import truediv
 from django.db import models
 
 from cloudinary.models import CloudinaryField
@@ -17,10 +18,10 @@ class Booking(models.Model):
     date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
-    table = models.ForeignKey(Table, on_delete=models.CASCADE, null = True)
-    customer_full_name = models.CharField(max_length=200)
-    customer_email = models.EmailField(max_length=200)
+    # table = models.ForeignKey(Table, on_delete=models.CASCADE, null = True)
+    customer_full_name = models.CharField(max_length=200, blank=True)
+    customer_email = models.EmailField(max_length=200, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
-
+       
     class Meta:
         ordering = ['date']
