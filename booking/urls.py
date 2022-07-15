210056c385c55e13296a7aff197597767b8ea69f
unknown <pal.iasmina@yahoo.com>
2022-07-15 22:23:54 +0100

    Change url path for booking view

--- booking/urls.py
+++ booking/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('createbookings', views.Booking.as_view(), name='booking'),
+    path('createbookings/', views.Booking.as_view(), name='booking'),
 ]
