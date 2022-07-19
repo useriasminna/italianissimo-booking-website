3e11e9b0281de13eaed111346f5e689cbc286a88
unknown <pal.iasmina@yahoo.com>
2022-07-19 15:50:20 +0100

    Update clean method for signup form

--- italianissimo/forms.py
+++ italianissimo/forms.py
@@ -6,12 +6,6 @@ class CustomSignupForm(SignupForm):
     first_name = forms.CharField(max_length=30, label='First Name', widget=forms.TextInput(attrs={'placeholder': 'First name'}))
     last_name = forms.CharField(max_length=30, label='Last Name', widget=forms.TextInput(attrs={'placeholder': 'Last name'}))
  
-    def save(self, request):
-        user = super(CustomSignupForm, self).save(request)
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
-        user.save()
-        return user
     
     def clean_first_name(self):
         first_name = self.cleaned_data['first_name']
@@ -20,6 +14,7 @@ class CustomSignupForm(SignupForm):
             raise forms.ValidationError("No special characters allowed")
         elif any(c.isdigit() for c in first_name):
             raise forms.ValidationError("No numbers allowed")
+        return first_name
         
     def clean_last_name(self):
         last_name = self.cleaned_data['last_name']
@@ -28,4 +23,11 @@ class CustomSignupForm(SignupForm):
             raise forms.ValidationError("No special characters allowed")
         elif any(c.isdigit() for c in last_name):
             raise forms.ValidationError("No numbers allowed")
+        return last_name
        
+    def save(self, request):
+        user = super(CustomSignupForm, self).save(request)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
+        return user
