1179719569e198c4f7dc7c1c28296c0691c237be
unknown <pal.iasmina@yahoo.com>
2022-07-20 20:55:32 +0100

    Add path for booking list and delete booking views

--- booking/urls.py
+++ booking/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('createbookings/', views.Booking.as_view(), name='booking'),
+    path('profile/', views.BookingList.as_view(), name='booking_list'),
+    path('profile/booking/<int:pk>/remove/', views.BookingDeleteView.as_view(), name='booking_remove')
 ]
