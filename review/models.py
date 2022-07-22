9a12ae20d8b7e01b198e8cfb55b08479fa8e1d10
unknown <pal.iasmina@yahoo.com>
2022-07-22 15:55:48 +0100

    Create review model

--- review/models.py
+++ review/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Review(models.Model):
+    rate = models.PositiveSmallIntegerField()
+    review_text = models.TextField()
+    date_created_on = models.DateTimeField(auto_now=True)
+    date_updated_on = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, to_field='email', blank=True)
+
+    
+    
+
+    class Meta:
+        ordering = ["date_updated_on"]
+
+    def __str__(self):
+        return f"Review {self.review_text} by {self.user}"
