50e447428fe099401e2786840e988f5ec413fe8d
unknown <pal.iasmina@yahoo.com>
2022-07-25 22:05:24 +0100

    Update model

--- review/models.py
+++ review/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.conf import settings
-from cloudinary.models import CloudinaryField
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 
 class Review(models.Model):
     rate = models.PositiveSmallIntegerField()
     review_text = models.TextField()
-    date_created_on = models.DateTimeField(auto_now=True)
-    date_updated_on = models.DateTimeField(auto_now_add=True)
+    date_created_on = models.DateTimeField(default=datetime.datetime.now)
+    date_updated_on = models.DateTimeField(default=datetime.datetime.now)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, to_field='email', blank=True)
 
     
