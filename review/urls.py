0e24ff438f233582634c9292c3b264a24c503393
unknown <pal.iasmina@yahoo.com>
2022-07-25 22:06:12 +0100

    Add url path for update view

--- review/urls.py
+++ review/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.Review.as_view(), name='reviews'),
+    path('review/<int:pk>/update/', views.ReviewUpdate.as_view(), name='review_update'),
     
     
 ]
