fafb82fb95652f794f5aabd7ebafc6ba14ee536c
unknown <pal.iasmina@yahoo.com>
2022-07-26 19:45:42 +0100

    Update home view with reviews list

--- home/views.py
+++ home/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from django.db.models import Q
+from review.models import Review 
+from users.models import User
+from django.db.models.functions import Length
 
 # Create your views here.
-class Home(TemplateView):
+class Home(ListView):
     template_name = "index.html"
+    model = Review
+
+    def get_context_data(self,*args, **kwargs):
+        context = super(Home, self).get_context_data(*args,**kwargs)
+        context['review_list'] = Review.objects.annotate(review_len=Length('review_text')).filter(review_len__lte=400).order_by('-rate')
+        context['users'] = User.objects.all() 
+        return context
