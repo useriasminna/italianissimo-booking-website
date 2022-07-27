064aa4ab0fd93fbecf655d9717c6fb5504f8f846
unknown <pal.iasmina@yahoo.com>
2022-07-27 12:51:22 +0100

    Update view name from BookingList to BookingMealsList

--- booking/urls.py
+++ booking/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('createbookings/', views.Booking.as_view(), name='booking'),
-    path('profile/', views.BookingList.as_view(), name='booking_list'),
+    path('profile/', views.BookingMealsList.as_view(), name='booking_list'),
     path('profile/booking/<int:pk>/remove/', views.BookingDeleteView.as_view(), name='booking_remove'),
     path('managebookings/', views.BookingListAdmin.as_view(), name='booking_list_admin'),
     path('managebookings/booking/<int:pk>/remove/', views.BookingDeleteViewAdmin.as_view(), name='booking_remove_admin'),
