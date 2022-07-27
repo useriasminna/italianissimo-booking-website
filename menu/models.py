9bc985ee0b36f10e94a357be9676ef6015bda659
unknown <pal.iasmina@yahoo.com>
2022-07-27 07:49:54 +0100

    Add models for Meal and Favourite

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,18 @@
+from operator import truediv
 from django.db import models
+import json
+from cloudinary.models import CloudinaryField
+from django.conf import settings
+from django.forms import JSONField
+
 
 # Create your models here.
+class Meal(models.Model):
+    name = models.CharField(max_length=100,unique=True)
+    cover = CloudinaryField('image')
+    price = models.FloatField()
+    ingredients = models.CharField(max_length=1000)
+
+class Favourite(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, to_field='email', blank=True)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE, blank=True)
