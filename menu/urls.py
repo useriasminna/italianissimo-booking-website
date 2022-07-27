6b0dc8b9c6e182e89b226b00e3d3dab77c694f13
unknown <pal.iasmina@yahoo.com>
2022-07-27 07:50:38 +0100

    Add url paths for menu listview and favourite delete view

---
+++ menu/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.Menu.as_view(), name='menu'),
+    path('favourite/<int:pk>/remove/', views.FavouriteDeleteView.as_view(), name='favourite_remove'),
+]
