070ed1442d6ed1d9014e8d8b1661ff77a340d042
unknown <pal.iasmina@yahoo.com>
2022-07-27 07:48:31 +0100

    Add script for menu page

--- static/js/script.js
+++ static/js/script.js
@@ -656,6 +656,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   }
 
+  if (window.location.pathname.includes('/menu/')) {
+    const mealIdList = document.getElementsByClassName('mealId')
+    const mealList = document.getElementsByClassName('meal')
+ 
+    //add value of meal id to favourite form field
+    for(input of mealIdList){
+      input.value = input.previousElementSibling.value
+    }
+
+    //display favourite forms depending on p hidden element value that is generated only if favourite exists for user and specific meal
+    for(meal of mealList){
+      console.log(meal)
+      const favouriteExists = meal.getElementsByClassName('favourite-exists')[0]
+      const addFavouriteForm = meal.getElementsByClassName('add-favourite-form')[0]
+      const removeFavouriteForm = meal.getElementsByClassName('remove-favourite-form')[0]
+
+      if(favouriteExists){
+        addFavouriteForm.style.display = 'none'
+      }
+
+    }
+
+
+  }
+
   
 
   
