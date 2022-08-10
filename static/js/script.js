9e6401155beced529d5739895778c61416ff13ed
unknown <pal.iasmina@yahoo.com>
2022-08-10 15:56:36 +0100

    Add map initialization method for contact page

--- static/js/script.js
+++ static/js/script.js
@@ -679,6 +679,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   }
 
+  if (window.location.pathname.includes('/contact/')) {
+
+    function initMap() {
+      // The location of Uluru
+      const uluru = { lat: 40.628920, lng: 14.487630 };
+      // The map, centered at Uluru
+      const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: uluru,
+      });
+      // The marker, positioned at Uluru
+      const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+      });
+    }
+    initMap()
+  }
+
   
 
   
