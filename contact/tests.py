af47ae534a4ee6a5d20be250e1b5407a89d34389
unknown <pal.iasmina@yahoo.com>
2022-08-16 12:17:53 +0100

    Add tests for contact page

--- contact/tests.py
+++ contact/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    """
+    Unit Tests for Contact App Views
+    """
+
+    def test_contact_page(self):
+        """ Test contact page renders correct page """
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact.html')
