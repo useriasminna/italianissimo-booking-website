db3e8af15dd0784952565607b71c3f89e5eeaa39
unknown <pal.iasmina@yahoo.com>
2022-08-18 15:04:28 +0100

    Update tests for home app

--- home/tests.py
+++ home/tests.py
@@ -11,3 +11,13 @@ class TestViews(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+        
+    def test_home_context(self):
+        """ Test if Reviews, Meals, Favourites and Users are rendered to Create Home page"""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('review_list' in response.context)
+        self.assertTrue('meals' in response.context)
+        self.assertTrue('fav' in response.context)
+        self.assertTrue('users' in response.context)
+
